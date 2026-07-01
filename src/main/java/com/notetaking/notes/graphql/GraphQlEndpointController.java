cc10b3b65ee33ea269e985665a5ca36b14216f01
Phillip Platt <pplatt@firmys.com>
2026-07-01 10:25:34 -0600

    fix: make GraphQlEndpointController always register the /graphql handler (optional service dep) to prevent 404 NOT_FOUND from DispatcherHandler

--- src/main/java/com/notetaking/notes/graphql/GraphQlEndpointController.java
+++ src/main/java/com/notetaking/notes/graphql/GraphQlEndpointController.java
@@ -3,12 +3,14 @@ package com.notetaking.notes.graphql;
 import org.springframework.graphql.ExecutionGraphQlResponse;
 import org.springframework.graphql.ExecutionGraphQlService;
 import org.springframework.graphql.support.DefaultExecutionGraphQlRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
@@ -23,12 +25,17 @@ public class GraphQlEndpointController {
 
   private final ExecutionGraphQlService executionGraphQlService;
 
-  public GraphQlEndpointController(ExecutionGraphQlService executionGraphQlService) {
+  public GraphQlEndpointController(@Autowired(required = false) ExecutionGraphQlService executionGraphQlService) {
     this.executionGraphQlService = executionGraphQlService;
   }
 
-  @PostMapping(value = "/graphql", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+  @PostMapping("/graphql")
   public Mono<Map<String, Object>> graphql(@RequestBody Map<String, Object> body) {
+    if (executionGraphQlService == null) {
+      return Mono.just(Map.of(
+        "errors", List.of(Map.of("message", "GraphQL execution service not available"))
+      ));
+    }
     String query = (String) body.get("query");
     @SuppressWarnings("unchecked")
     Map<String, Object> variables = (Map<String, Object>) body.getOrDefault("variables", Map.of());
